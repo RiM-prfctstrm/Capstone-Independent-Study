9c93073bf309bfbdfca45fb6217df41a57df1444
RiM-prfctstrm <78179573+RiM-prfctstrm@users.noreply.github.com>
2024-09-10 11:42:08 -0500

    Built walking movement and adjusted screen size.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,13 +2,14 @@
  * FILE     : PlayerController.cs
  * AUTHOR   : Peter "prfctstrm479" Campbell
  * CREATION : 8/27/24
- * UPDATED  : 8/27/24
+ * UPDATED  : 9/10/24
  * 
- * DESC     : Controls the player character
+ * DESC     : Controls the player character's movement and world interactions.
 =================================================================================================*/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -26,7 +27,14 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float _decelRate;
     [SerializeField] bool _isWalking;
 
-    //
+    //Movement Vars
+    float _velocityX;
+    float _velocityY;
+
+    //Inputs
+    [SerializeField] InputActionAsset _playerInputs;
+    InputAction _moveX;
+    InputAction _moveY;
 
     /// <summary>
     /// Start is called before the first frame update
@@ -37,6 +45,10 @@ public class PlayerController : MonoBehaviour
         _playerAnimator = GetComponent<Animator>();
         _playerRenderer = GetComponent<SpriteRenderer>();
         _rb2D = GetComponent<Rigidbody2D>();
+
+        // Sets inputs
+        _moveX = _playerInputs.FindAction("MoveX");
+        _moveY = _playerInputs.FindAction("MoveY");
     }
 
     /// <summary>
@@ -47,11 +59,32 @@ public class PlayerController : MonoBehaviour
         // Controls which kind of movement to perform.
         if (_isWalking)
         {
-
+            WalkMovement();
         }
         else
         {
-
+            BikeMovement();
         }
+    }
+
+    /// <summary>
+    /// Controls the player's movement while walking
+    /// </summary>
+    void WalkMovement()
+    {
+        // Sets velocity components
+        _velocityX = _walkSpeed * (int)_moveX.ReadValue<float>() * Time.deltaTime;
+        _velocityY = _walkSpeed * (int)_moveY.ReadValue<float>() * Time.deltaTime;
+
+        // Sets Player's velocity
+        _rb2D.velocity = new Vector2(_velocityX, _velocityY);
+    }
+
+    /// <summary>
+    /// Controls the player's movement while biking
+    /// </summary>
+    void BikeMovement()
+    {
+
     }
 }
