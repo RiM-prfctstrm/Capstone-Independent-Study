8deb880ca17f533e5fde941cb79e6d1af46a3aa5
RiM-prfctstrm <78179573+RiM-prfctstrm@users.noreply.github.com>
2024-09-10 16:49:22 -0500

    Began working on NPC and Player Animations

---
+++ Assets/Scripts/NPCs/CharacterAnimator.cs
@@ -0,0 +1,41 @@
+/*=================================================================================================
+ * FILE     : CharacterAnimator.cs
+ * AUTHOR   : Peter "prfctstrm479" Campbell
+ * CREATION : 9/10/24
+ * UPDATED  : 9/10/24
+ * 
+ * DESC     : Base code to automate NPC animations.
+=================================================================================================*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    /* Vars */
+    public int facingDirection; // 0=D, 1=L, 2=R, 3=U
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Determines which animation to play
+    /// </summary>
+    void SetAnimState()
+    {
+
+    }
+}
