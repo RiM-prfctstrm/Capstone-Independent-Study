e63ae9267d02f7c917d1f1fa5faf7afeb5a418a2
RiM-prfctstrm <78179573+RiM-prfctstrm@users.noreply.github.com>
2024-09-19 11:29:23 -0500

    Began programming bike acceleration

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -23,13 +23,15 @@ public class PlayerController : MonoBehaviour
     //Parameters
     [SerializeField] float _walkSpeed;
     [SerializeField] float _maxBikeSpeed;
-    [SerializeField] float _accelRate;
-    [SerializeField] float _decelRate;
+    [SerializeField] float _accelTime;
+    [SerializeField] float _decelTime;    
     public bool _isWalking;
 
     //Movement Vars
     float _velocityX;
     float _velocityY;
+    float _accelRate;
+    float _decelRate;
 
     //Inputs
     [SerializeField] InputActionAsset _playerInputs;
@@ -49,6 +51,9 @@ public class PlayerController : MonoBehaviour
         // Sets inputs
         _moveX = _playerInputs.FindAction("MoveX");
         _moveY = _playerInputs.FindAction("MoveY");
+
+        // Initializes Accel/Decel Rates
+        SetAccelDecel();
     }
 
     /// <summary>
@@ -79,8 +84,8 @@ public class PlayerController : MonoBehaviour
     void WalkMovement()
     {
         // Sets velocity components
-        _velocityX = _walkSpeed * (int)_moveX.ReadValue<float>() * Time.deltaTime;
-        _velocityY = _walkSpeed * (int)_moveY.ReadValue<float>() * Time.deltaTime;
+        _velocityX = _walkSpeed * (int)_moveX.ReadValue<float>() * Time.fixedDeltaTime;
+        _velocityY = _walkSpeed * (int)_moveY.ReadValue<float>() * Time.fixedDeltaTime;
 
         // Sets Player's velocity
         _rb2d.velocity = new Vector2(_velocityX, _velocityY);
@@ -91,7 +96,42 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     void BikeMovement()
     {
+        /* DETERMINES INITIAL VELOCITY COMPONENTS */
+        // X component
+        if ((int)_moveX.ReadValue<float>() != 0)
+        {
+            _velocityX += _accelRate * (int)_moveX.ReadValue<float>() * Time.fixedDeltaTime;
+            Mathf.Clamp(_velocityX, 0, _maxBikeSpeed);
+        }
+        /*else if (_rb2d.velocity.x != 0)
+        {
+            // Braking logic might go here
+            _velocityX -= _decelRate * Time.deltaTime;
+        }*/
 
+        // Y component
+        if ((int)_moveY.ReadValue<float>() != 0)
+        {
+            _velocityY += _accelRate * (int)_moveY.ReadValue<float>() * Time.fixedDeltaTime;
+            Mathf.Clamp(_velocityY, 0, _maxBikeSpeed);
+        }
+        /*else if (_rb2d.velocity.y != 0)
+        {
+            // Braking logic might go here
+            _velocityY -= _decelRate * Time.deltaTime;
+        } */
+
+        // Multidirectional logic
+
+
+        /*// Determines velocity sign
+        if ((int)_moveX.ReadValue<float>() == -1 || _rb2d.velocity.x < 0)
+            _velocityX *= -1;
+        if ((int)_moveY.ReadValue<float>() == -1 || _rb2d.velocity.y < 0)
+            _velocityY *= -1;*/
+
+        // Sets Player's velocity
+        _rb2d.velocity = new Vector2(_velocityX, _velocityY);
     }
 
     /// <summary>
@@ -115,6 +155,16 @@ public class PlayerController : MonoBehaviour
                 return 3;
             else
                 return 0;
-        }           
+        }
+    }
+
+    /// <summary>
+    /// Sets the rates at which the bike accelerates and decelerates
+    /// </summary>
+    void SetAccelDecel()
+    {
+        _accelRate = (_maxBikeSpeed / _accelTime) * Time.fixedDeltaTime;
+        _decelRate = (_maxBikeSpeed / _decelTime) * Time.fixedDeltaTime;
+        Debug.Log(_accelRate + ", " + _decelRate);
     }
 }
