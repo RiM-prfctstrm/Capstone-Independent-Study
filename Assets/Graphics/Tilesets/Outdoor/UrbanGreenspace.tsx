9cc75f75582d62f349ce811c82288b9b5b69421c
RiM-prfctstrm <78179573+RiM-prfctstrm@users.noreply.github.com>
2024-11-23 13:39:46 -0600

    Finished Mapping First level BG

---
+++ Assets/Graphics/Tilesets/Outdoor/UrbanGreenspace.tsx
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="UrbanGreenspace" tilewidth="16" tileheight="16" tilecount="25" columns="5">
+ <image source="UrbanGreenspace.png" width="80" height="80"/>
+ <tile id="13" probability="0.05"/>
+ <tile id="14" probability="0.01"/>
+</tileset>
