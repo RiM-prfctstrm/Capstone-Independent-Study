bdc6a1b69194c8a83cff2ce62b68d2a6218a3e71
RiM-prfctstrm <78179573+RiM-prfctstrm@users.noreply.github.com>
2024-12-07 16:30:38 -0600

    Started revising Dylan's scene and decorating interiors

---
+++ Assets/Graphics/Tilesets/Indoor/Furniture.tsx
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Furniture" tilewidth="16" tileheight="16" tilecount="56" columns="8">
+ <image source="Furniture.png" width="128" height="112"/>
+ <tile id="0">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
+</tileset>
