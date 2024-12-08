b23d03ec53c76dad24c808a60662992510d26d1a
RiM-prfctstrm <78179573+RiM-prfctstrm@users.noreply.github.com>
2024-12-08 07:11:40 -0600

    Started alternate movement script

---
+++ Assets/Scripts/Cutscenes/CutsceneEvents/CharacterControls/MoveToPoint.cs
@@ -0,0 +1,24 @@
+/*=================================================================================================
+ * FILE     : MoveToPoints.cs
+ * AUTHOR   : Peter "prfctstrm479" Campbell
+ * CREATION : 12/8/24
+ * UPDATED  : 12/8/24
+ * 
+ * DESC     : Moves a character to a specified point.
+=================================================================================================*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Movement Destination",
+    menuName = "Cutscene/Character Controls/Movement Destinations", order = 3)]
+public class MoveToPoint : CutsceneEvent
+{
+    #region VARIABLES
+
+    #endregion
+
+    #region EVENT FUNCTIONALITY
+
+    #endregion
+}
