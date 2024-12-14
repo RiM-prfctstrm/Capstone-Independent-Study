c403d9fb2c71b8aad3fcc6c6c526ac1b188652b4
RiM-prfctstrm <78179573+RiM-prfctstrm@users.noreply.github.com>
2024-12-14 17:34:26 -0600

    Built Scene for Sage

--- Assets/Graphics/Maps/Retail/Restaurant.tsx
+++ Assets/Graphics/Maps/Retail/Restaurant.tsx
@@ -1,4 +1,49 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Restaurant" tilewidth="16" tileheight="16" tilecount="30" columns="6">
- <image source="../../Tilesets/Indoor/Restaurant.png" width="96" height="80"/>
+<tileset version="1.10" tiledversion="1.10.2" name="Restaurant" tilewidth="16" tileheight="16" tilecount="48" columns="8">
+ <image source="../../Tilesets/Indoor/Restaurant.png" width="128" height="96"/>
+ <tile id="2">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="unity:SortingLayer" value="Default"/>
+  </properties>
+ </tile>
 </tileset>
