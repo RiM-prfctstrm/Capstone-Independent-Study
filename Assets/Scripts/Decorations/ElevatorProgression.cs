342b46cb283c71e9fcab3f86699f164f536a52db
RiM-prfctstrm <pjpciii@gmail.com>
2025-01-09 13:00:56 -0600

    Elevator progresses through multiple backgrounds

--- Assets/Scripts/Decorations/ElevatorProgression.cs
+++ Assets/Scripts/Decorations/ElevatorProgression.cs
@@ -17,10 +17,26 @@ public class ElevatorProgression : MonoBehaviour
 {
     #region VARIABLES
 
-    // Start is called before the first frame update
+    // Tracking variables
+    public int polarity = 1; // 1 upward, -1 downward
+    int _level = -1; //Tracks execution order
+
+    // External References
+    [SerializeField] Sprite[] _backGrounds;
+    [SerializeField] SpriteRenderer _bgObject;
+
+    #endregion
+
+    #region UNIVERSAL EVENTS
+
+    /// <summary>
+    ///  Start is called before the first frame update
+    /// </summary>
     void Start()
     {
-        
+        // Ignores collisions with player
+        /*Physics2D.IgnoreCollision(GetComponent<Collider2D>(),
+            PlayerController.playerController.GetComponent<Collider2D>());*/
     }
 
     // Update is called once per frame
@@ -28,4 +44,40 @@ public class ElevatorProgression : MonoBehaviour
     {
         
     }
+
+    /// <summary>
+    /// OnTriggerEnter2D is called when the Collider2d other enters the trigger
+    /// </summary>
+    /// <param name="collision">Object that collides with this</param>
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Sets Current Elevator level
+        _level += polarity;
+
+        // Changes elevator state when the trigger object reaches this point
+        switch (_level)
+        {
+            // Sends player to Ground Floor
+            case -1:
+
+                break;
+
+            // Changes background, nothing else
+            case <= 2:
+                _bgObject.sprite = _backGrounds[_level];
+                break;
+
+            // Sends Player to Top Floor
+            case 3:
+                Debug.Log("Made it!");
+                break;
+        }
+
+        /*if (collision.CompareTag(gameObject.tag))
+        {
+
+        }*/
+    }
+
+    #endregion
 }
