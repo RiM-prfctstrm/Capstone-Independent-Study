4f79bcb44c19bf3130dbcf0e2d5175335d75b7e3
RiM-prfctstrm <pjpciii@gmail.com>
2025-01-17 15:28:58 -0600

    Added sceen transition to BottleMail

--- Assets/Scripts/Menus/BottleMailMenu.cs
+++ Assets/Scripts/Menus/BottleMailMenu.cs
@@ -69,6 +69,9 @@ public class BottleMailMenu : MonoBehaviour
         {
             _scrollBar.size = 1;
         }
+
+        // Auto selects scrollbar
+        _scrollBar.Select();
     }
 
     #endregion
