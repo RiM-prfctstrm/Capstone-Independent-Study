604c2c60a3ba067701d166566e3c1d23122b55c2
RiM-prfctstrm <pjpciii@gmail.com>
2025-01-25 14:11:27 -0600

    Set up save file workspace

--- Assets/Scripts/Progression/GlobalVariableTracker.cs
+++ Assets/Scripts/Progression/GlobalVariableTracker.cs
@@ -2,7 +2,7 @@
  * FILE     : GlobalVariableTracker.cs
  * AUTHOR   : Peter "prfctstrm479" Campbell
  * CREATION : 11/4/24
- * UPDATED  : 11/4/24
+ * UPDATED  : 1/25/25
  * 
  * DESC     : Stores data that is meant to persist throughout the entire game. Variables are kept
  *            in an initialized state to easily create a new save. If a save is loaded, they are
@@ -21,6 +21,14 @@ public static class GlobalVariableTracker
 
     // General mission information
     public static int currentMission = 0;
+
+    #endregion
+
+    #region PERSISTANT FLAGS
+
+    // Cutscene progress variables
+
+    // Door locking variables
 
     #endregion
 
---
+++ Assets/Scripts/Progression/SaveManager.cs
@@ -0,0 +1,40 @@
+/*=================================================================================================
+ * FILE     : GlobalVariableTracker.cs
+ * AUTHOR   : Peter "prfctstrm479" Campbell
+ * CREATION : 1/25/25
+ * UPDATED  : 1/25/25
+ * 
+ * DESC     : Saves and loads data in a persistant format.
+=================================================================================================*/
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public static class SaveManager
+{
+    #region SAVING FUNCTIONS
+
+    public static void SaveGame()
+    {
+
+    }
+
+    #endregion
+
+    #region LOADING FUNCTIONS
+
+    public static void LoadGame()
+    {
+
+    }
+
+    public static void ResetSave()
+    {
+
+    }
+
+    #endregion
+}
