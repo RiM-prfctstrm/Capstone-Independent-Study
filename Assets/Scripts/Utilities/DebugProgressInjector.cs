7b863c0f65003fcd8b7d59a950e677ad9dc8cd6f
RiM-prfctstrm <pjpciii@gmail.com>
2025-02-03 15:51:21 -0600

    Set up warp to space elevator

---
+++ Assets/Scripts/Utilities/DebugProgressInjector.cs
@@ -0,0 +1,79 @@
+/*=================================================================================================
+ * FILE     : DebugProgressInjector.cs
+ * AUTHOR   : Peter "prfctstrm479" Campbell
+ * CREATION : 2/3/25
+ * UPDATED  : 2/3/25
+ * 
+ * DESC     : Debug script to set progression variables by hand in inspector. Used to tell the game
+ *            to play at a certain point. Works best before loading scene.
+=================================================================================================*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugProgressInjector : MonoBehaviour
+{
+
+    #region VARIABLES
+
+    // Mission status
+    [SerializeField] int _currentMission;
+    [SerializeField] bool _m0Complete = false;
+    [SerializeField] bool _m1Complete = false;
+    [SerializeField] bool _m2Complete = false;
+    [SerializeField] bool _m3Complete = false;
+
+
+    // Activation Switch
+    [SerializeField] bool _fireInjector = false;
+
+    #endregion
+
+    #region UNIVERSAL EVENTs
+    /// <summary>
+    /// Awake is called when the script instance is first loaded
+    /// </summary>
+    void Awake()
+    {
+        // Allows continued debugging across scenes
+        DontDestroyOnLoad(gameObject);
+
+        // Fires injector
+        InjectGlobalData();
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        // Fires injector
+        if (_fireInjector)
+        {
+            InjectGlobalData();
+        }
+    }
+
+    #endregion
+
+    #region SETTER FUNCTION
+
+    /// <summary>
+    /// Sets global variables to equal parameters set in script
+    /// </summary>
+    void InjectGlobalData()
+    {
+        // Mission Data
+        GlobalVariableTracker.currentMission = _currentMission;
+        GlobalVariableTracker.m0Complete = _m0Complete;
+        GlobalVariableTracker.m1Complete = _m1Complete;
+        GlobalVariableTracker.m2Complete = _m2Complete;
+        GlobalVariableTracker.m3Complete = _m3Complete;
+
+        // Prevent repeat fires
+        _fireInjector = false;
+
+    }
+
+    #endregion
+}
