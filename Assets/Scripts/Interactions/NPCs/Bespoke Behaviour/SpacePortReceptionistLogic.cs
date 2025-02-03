a4e54ebbc7a9b6fd487575b51632b66164759b1a
RiM-prfctstrm <pjpciii@gmail.com>
2025-02-03 09:30:18 -0600

    Began Work on Space Elevator Reception Area

---
+++ Assets/Scripts/Interactions/NPCs/Bespoke Behaviour/SpacePortReceptionistLogic.cs
@@ -0,0 +1,27 @@
+/*=================================================================================================
+ * FILE     : SpacePortReceptionistLogic
+ * AUTHOR   : Peter "prfctstrm479" Campbell
+ * CREATION : 2/3/25
+ * UPDATED  : 2/3/25
+ * 
+ * DESC     : Controls behaviour of the Spaceport receptionist, based on the current state of
+ *            external variables.
+=================================================================================================*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpacePortReceptionistLogic : NPCInteraction
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
