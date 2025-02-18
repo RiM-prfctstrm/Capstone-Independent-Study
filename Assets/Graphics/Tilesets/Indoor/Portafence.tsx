0d4ea14756494ba9bdb1e2f5778de6ba53a0d3c8
RiM-prfctstrm <pjpciii@gmail.com>
2025-02-18 12:30:42 -0600

    Added entrance barrier to spaceport

---
+++ Assets/Graphics/Tilesets/Indoor/Portafence.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Portafence" tilewidth="16" tileheight="16" tilecount="12" columns="6">
+ <image source="Portafence.png" width="96" height="32"/>
+</tileset>
