bce480321dc3ba3f5c5f5a412455bd35c1a9f105
RiM-prfctstrm <pjpciii@gmail.com>
2025-02-18 13:26:01 -0600

    Added Water Graphics

---
+++ Assets/Graphics/Tilesets/Outdoor/Water.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Water" tilewidth="16" tileheight="16" tilecount="16" columns="16">
+ <image source="Water.png" width="256" height="16"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="250"/>
+   <frame tileid="1" duration="250"/>
+   <frame tileid="2" duration="250"/>
+   <frame tileid="3" duration="250"/>
+   <frame tileid="4" duration="250"/>
+   <frame tileid="5" duration="250"/>
+   <frame tileid="6" duration="250"/>
+   <frame tileid="7" duration="250"/>
+   <frame tileid="8" duration="250"/>
+   <frame tileid="9" duration="250"/>
+   <frame tileid="10" duration="250"/>
+   <frame tileid="11" duration="250"/>
+   <frame tileid="12" duration="250"/>
+   <frame tileid="13" duration="250"/>
+   <frame tileid="14" duration="250"/>
+   <frame tileid="15" duration="250"/>
+  </animation>
+ </tile>
+</tileset>
