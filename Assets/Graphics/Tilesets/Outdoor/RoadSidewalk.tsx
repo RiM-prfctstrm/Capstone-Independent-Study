66b5793f514ae19435999b59866d732b1c703033
RiM-prfctstrm <pjpciii@gmail.com>
2025-02-27 18:16:45 -0600

    attempted collectible layout

--- Assets/Graphics/Tilesets/Outdoor/RoadSidewalk.tsx
+++ Assets/Graphics/Tilesets/Outdoor/RoadSidewalk.tsx
@@ -1,4 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="RoadSidewalk" tilewidth="16" tileheight="16" tilecount="72" columns="9">
+<tileset version="1.10" tiledversion="1.11.0" name="RoadSidewalk" tilewidth="16" tileheight="16" tilecount="72" columns="9">
  <image source="RoadSidewalkTileset.png" width="144" height="128"/>
+ <tile id="0" probability="0.25"/>
+ <tile id="9" probability="0.25"/>
+ <tile id="18" probability="0.25"/>
+ <tile id="27" probability="0.25"/>
 </tileset>
