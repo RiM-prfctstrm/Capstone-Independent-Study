d4b0821e6006f89983cf60699385e194c3d609b5
RiM-prfctstrm <pjpciii@gmail.com>
2025-03-03 15:41:33 -0600

    Added graphics to elevator base

--- Assets/Graphics/Tilesets/Outdoor/SpaceElevatorIndustrial.tsx
+++ Assets/Graphics/Tilesets/Outdoor/SpaceElevatorIndustrial.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="SpaceElevatorIndustrial" tilewidth="16" tileheight="16" tilecount="91" columns="13">
- <image source="SpaceElevatorIndustrial.png" width="208" height="112"/>
+<tileset version="1.10" tiledversion="1.11.0" name="SpaceElevatorIndustrial" tilewidth="16" tileheight="16" tilecount="112" columns="16">
+ <image source="SpaceElevatorIndustrial.png" width="256" height="112"/>
 </tileset>
