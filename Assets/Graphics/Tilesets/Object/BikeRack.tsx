f0d5290e09c50a06d9f86ac8e78eac59dc97cc4a
RiM-prfctstrm <pjpciii@gmail.com>
2025-03-17 18:01:31 -0500

    final polish for real

--- Assets/Graphics/Tilesets/Object/BikeRack.tsx
+++ Assets/Graphics/Tilesets/Object/BikeRack.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="BikeRack" tilewidth="16" tileheight="16" tilecount="3" columns="3">
- <image source="BikeRack.png" width="48" height="16"/>
+<tileset version="1.10" tiledversion="1.11.0" name="BikeRack" tilewidth="16" tileheight="16" tilecount="6" columns="3">
+ <image source="BikeRack.png" width="48" height="32"/>
 </tileset>
