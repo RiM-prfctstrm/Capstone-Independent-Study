f2838bf3e1dc23db8e05341e878c78eeb7bda697
RiM-prfctstrm <pjpciii@gmail.com>
2025-08-22 10:45:17 -0500

    Set up script for nanoblip

---
+++ Assets/Scripts/Collectibles/Nanoblip.cs
@@ -0,0 +1,38 @@
+/*=================================================================================================
+ * FILE     : Nanoblip.cs
+ * AUTHOR   : Peter "prfctstrm479" Campbell
+ * CREATION : 8/22/25
+ * UPDATED  : 8/22/25
+ * 
+ * DESC     : Behaviour for items that can be picked up off the ground.
+=================================================================================================*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nanoblip : Collectible
+{
+    #region VARS
+
+    #endregion
+
+    #region UNIVERSAL EVENTS
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    void Start()
+    {
+
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+
+    }
+
+    #endregion
+}
