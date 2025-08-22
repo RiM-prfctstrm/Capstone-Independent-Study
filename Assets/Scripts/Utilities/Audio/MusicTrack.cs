9257895a268c3cc609a19f0901f6de66eb53f056
RiM-prfctstrm <pjpciii@gmail.com>
2025-08-22 10:59:00 -0500

    Set up new system for handling music

---
+++ Assets/Scripts/Utilities/Audio/MusicTrack.cs
@@ -0,0 +1,32 @@
+/*=================================================================================================
+ * FILE     : MusicTrack.cs
+ * AUTHOR   : Peter "prfctstrm479" Campbell
+ * CREATION : 8/22/25
+ * UPDATED  : 8/22/25
+ * 
+ * DESC     : Stores data for music to control playback by the song
+=================================================================================================*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class MusicTrack : ScriptableObject
+{
+    #region VARS
+
+    // Song Audio
+    [SerializeField] AudioClip _song;
+    public AudioClip song => _song;
+
+    // Control parameters
+    //[SerializeField] bool _loops = true;
+    [SerializeField] int _loopBeat = 0;
+    public int loopBeat => _loopBeat;
+
+    // Information used to calculate precise timing
+    [SerializeField] int _bpm;
+    public int bpm => _bpm;
+
+    #endregion
+}
