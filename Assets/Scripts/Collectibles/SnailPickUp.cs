723bf7c1a50270408e8ef02065a418f679319645
RiM-prfctstrm <pjpciii@gmail.com>
2025-09-22 13:03:10 -0500

    Programmed Basic Snail movement

--- Assets/Scripts/Collectibles/SnailPickUp.cs
+++ Assets/Scripts/Collectibles/SnailPickUp.cs
@@ -17,6 +17,10 @@ public class SnailPickUp : Collectible
     #region VARIABLES
 
     // Movement
+    int _direction = 1;
+    float _moveRate;
+    [SerializeField] float _speed;
+    [SerializeField] bool _vertical;
 
     #endregion
 
@@ -39,15 +43,55 @@ public class SnailPickUp : Collectible
     /// </summary>
     void Start()
     {
-        
+        // Inits vars
+        _moveRate = _speed * _direction * Time.deltaTime;
     }
 
     /// <summary>
-    /// Update is called once per frame
+    /// FixedUpdate is called every fixed framerate frame
     /// </summary>
-    void Update()
+    void FixedUpdate()
     {
-        
+        // Performs movement
+        MoveSnail();
+    }
+
+    #endregion
+
+    #region COLLISION CONTROLS
+
+    /// <summary>
+    /// Sent when an incoming collider makes contact with this object's collider (2D physics only).
+    /// </summary>
+    /// <param name="collision">The Collision2D data associated with this collision.</param>
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Changes movement direction
+        _direction = -_direction;
+        _moveRate = _speed * _direction * Time.deltaTime;
+
+        // Changes sprite orientation
+        GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
+    }
+
+    #endregion
+
+    #region MOVEMENT FUNCTIONALITY
+
+    /// <summary>
+    /// Moves the snail
+    /// </summary>
+    void MoveSnail()
+    {
+        // Sets relocation position
+        if (!_vertical)
+        {
+            transform.Translate(new Vector2(_moveRate, 0));
+        }
+        else
+        {
+            transform.Translate(new Vector2(0, _moveRate));
+        }
     }
 
     #endregion
