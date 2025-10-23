52f76cd4b56b1a24e8b8438ff64e7fa93cb3b05b
RiM-prfctstrm <pjpciii@gmail.com>
2025-10-23 14:01:55 -0500

    Attempted to program multicamera

---
+++ Assets/Scripts/Utilities/FrameControls.cs
@@ -0,0 +1,52 @@
+/*=================================================================================================
+ * FILE     : FrameControls.cs
+ * AUTHOR   : Peter "prfctstrm479" Campbell
+ * CREATION : 10/23/25
+ * UPDATED  : 10/23/25
+ * 
+ * DESC     : Changes the game window from just showing the game to surrounding it with a
+              fullscreen border, and vice versa.
+=================================================================================================*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class FrameControls : MonoBehaviour
+{
+    #region VARIABLES
+
+    // Cameras
+    [SerializeField] Camera _frameCamera;
+    [SerializeField] Camera _gameCamera;
+
+    #endregion
+
+    #region UNIVERSAL EVENTS
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded
+    /// </summary>
+    void Awake()
+    {
+        EnableFullscreen();
+    }
+
+    #endregion
+
+    #region SCREEN MODIFICATION
+
+    /// <summary>
+    /// Sets the application to fullscreen and ensures the regular camera is functioning properly
+    /// </summary>
+    void EnableFullscreen()
+    {
+        // Full screens game
+        Screen.fullScreen = true;
+
+        // Aligns and centers gamewindow
+        _gameCamera.ViewportToScreenPoint(new Vector2(.5f, .5f));
+    }
+
+    #endregion
+}
