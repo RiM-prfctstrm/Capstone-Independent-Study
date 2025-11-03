e6e3ad4c382bb84426761e86383d6689a162a417
RiM-prfctstrm <pjpciii@gmail.com>
2025-11-03 12:36:18 -0600

    Made menu move to match selected button

---
+++ Assets/Scripts/Menus/Main/ButtonCycler.cs
@@ -0,0 +1,27 @@
+/*=================================================================================================
+ * FILE     : ButtonCycler.cs
+ * AUTHOR   : Peter "prfctstrm479" Campbell
+ * CREATION : 11/3/25
+ * UPDATED  : 11/3/25
+ * 
+ * DESC     : Slides menu canvas to a specified point.
+=================================================================================================*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI
+
+public class ButtonCycler : MonoBehaviour
+{
+
+
+    #region UNIVERSAL EVENTS
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #endregion
+}
--- Assets/Scripts/Menus/Main/SlideMenu.cs
+++ Assets/Scripts/Menus/Main/SlideMenu.cs
@@ -1,18 +1,73 @@
+/*=================================================================================================
+ * FILE     : SlideMenu.cs
+ * AUTHOR   : Peter "prfctstrm479" Campbell
+ * CREATION : 11/3/25
+ * UPDATED  : 11/3/25
+ * 
+ * DESC     : Slides menu canvas to a specified point.
+=================================================================================================*/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SlideMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    #region VARIABLES
 
-    // Update is called once per frame
+    // Position Vars
+    [SerializeField] int _heldcoord;
+    Vector2 _start;
+    Vector2 _target;
+
+    // Speed Vars
+    bool _inMove = false;
+    float _duration = .1f;
+    float _elapsed;
+
+    #endregion
+
+    #region UNIVERSAL EVENTS
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
     void Update()
     {
-        
+        // Slides object position
+        if (_inMove)
+        {
+            GetComponent<RectTransform>().anchoredPosition =
+                Vector2.Lerp(_start, _target, _elapsed / _duration);
+            _elapsed += Time.deltaTime;
+        }
+
+        // Ends movement
+        if (_elapsed > _duration)
+        {
+            _inMove = false;
+        }
     }
+
+    #endregion
+
+    #region BUTTON ACTIONS
+
+    /// <summary>
+    /// Sets point for the menu to slide to
+    /// </summary>
+    /// <param name="pos">The position to slide to</param>
+    public void SlideToPos(int pos)
+    {
+        // Vars
+        RectTransform rt = GetComponent<RectTransform>();
+
+        // Sets parameters
+        _start = rt.anchoredPosition;
+        _target = new Vector2(pos, _heldcoord);
+        _elapsed = 0;
+        _inMove = true;
+    }
+
+    #endregion
 }
