cd57efb28117766ccddd025a5e309f3691ce2a2e
RiM-prfctstrm <pjpciii@gmail.com>
2025-11-11 08:02:56 -0600

    Removed inesssential menus from options

---
+++ Assets/Scripts/Menus/Submenus/SaveSlotIcon.cs
@@ -0,0 +1,30 @@
+/*=================================================================================================
+ * FILE     : SaveSlotIcon.cs
+ * AUTHOR   : Peter "prfctstrm479" Campbell
+ * CREATION : 11/10/25
+ * UPDATED  : 11/10/25
+ * 
+ * DESC     : Displays progress markers on a save slot button
+=================================================================================================*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveSlotIcon : MonoBehaviour
+{
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        
+    }
+}
