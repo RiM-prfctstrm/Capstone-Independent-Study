1db2aecd63dbecf264e32f5607280bfdfd2b4481
RiM-prfctstrm <pjpciii@gmail.com>
2026-03-10 12:56:48 -0500

    Updated office tileset

--- Assets/Graphics/Tilesets/Indoor/Office.tsx
+++ Assets/Graphics/Tilesets/Indoor/Office.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Office" tilewidth="16" tileheight="16" tilecount="80" columns="10">
- <image source="Office.png" width="160" height="128"/>
+<tileset version="1.10" tiledversion="1.11.0" name="Office" tilewidth="16" tileheight="16" tilecount="100" columns="10">
+ <image source="Office.png" width="160" height="160"/>
 </tileset>
