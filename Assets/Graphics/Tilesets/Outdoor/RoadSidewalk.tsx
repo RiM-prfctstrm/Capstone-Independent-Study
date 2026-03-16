fcf669b45f748562c8fab68f90c69ce7693ed408
RiM-prfctstrm <pjpciii@gmail.com>
2026-03-16 13:22:43 -0500

    Added road details to forest district

--- Assets/Graphics/Tilesets/Outdoor/RoadSidewalk.tsx
+++ Assets/Graphics/Tilesets/Outdoor/RoadSidewalk.tsx
@@ -5,4 +5,8 @@
  <tile id="9" probability="0.25"/>
  <tile id="18" probability="0.25"/>
  <tile id="27" probability="0.25"/>
+ <tile id="42" probability="0.125"/>
+ <tile id="52" probability="0.125"/>
+ <tile id="61" probability="0.125"/>
+ <tile id="69" probability="0.125"/>
 </tileset>
