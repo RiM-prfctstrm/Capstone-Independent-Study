f5c69930fc92658e1310301605193c28225c240e
RiM-prfctstrm <pjpciii@gmail.com>
2026-03-27 15:24:21 -0500

    Drafted achievement code

--- Assets/Scripts/Utilities/SteamManager.cs
+++ Assets/Scripts/Utilities/SteamManager.cs
@@ -11,12 +11,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using Steamworks;
 
+public enum eAchievements { ACH_FIRST_SNAIL }
 public class SteamManager : MonoBehaviour
 {
     #region VARS
 
+    // Class Singleton
+    public static SteamManager steamManager;
+
     // App Info
-    public uint appId = 4450980;
+    uint appId = 4450980;
+    bool _connectedToSteam = false;
+
+    // Achievement info
+    //int _achTotal = 2;
 
     #endregion
 
@@ -27,17 +35,29 @@ public class SteamManager : MonoBehaviour
     /// </summary>
     void Awake()
     {
-        DontDestroyOnLoad(this);
+        // Prepares single instance
+        if (steamManager == null)
+        {
+            steamManager = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
 
         // Activates Steam Client
         try
         {
             SteamClient.Init(appId);
+            _connectedToSteam = true;
             Debug.Log("Mmmm... Steamed Hams");
         }
         catch (System.Exception e)
         {
             // Couldn't init for some reason (steam is closed etc)
+            _connectedToSteam = false;
+            Debug.LogError(e);
         }
     }
 
@@ -46,7 +66,50 @@ public class SteamManager : MonoBehaviour
     /// </summary>
     void Update()
     {
-        
+        // Calls Steam client
+        if (_connectedToSteam)
+        {
+            SteamClient.RunCallbacks();
+        }
+    }
+
+    /// <summary>
+    /// This function is called when the behavior becomes disabled or inactive
+    /// </summary>
+    private void OnDisable()
+    {
+        // Shuts down steam client
+        SteamClient.Shutdown();
+    }
+
+    #endregion
+
+    #region ACHIEVEMENTS
+
+    /// <summary>
+    /// Unlocks a Steam Achievement
+    /// </summary>
+    /// <param name="ach">The Acheivement to Unlock</param>
+    public void UnlockAchievement(string achName)
+    {
+        // Ensures Steam is Running
+        if (_connectedToSteam)
+        {
+            // Sets achievement
+            var ach = new Steamworks.Data.Achievement(achName);
+            
+            // Awards achievement
+            if (!ach.State)
+            {
+                Debug.Log("alreadyObtained");
+            }
+            else
+            {
+                ach.Trigger();
+                Debug.Log(achName + " Achieved!");
+            }
+        }
+
     }
 
     #endregion
