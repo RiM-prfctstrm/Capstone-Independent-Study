7e5c001f3b6acd50f0077431ea999afc90b23ac4
prfctstrm479 <pjpciii@gmail.com>
2026-06-22 16:12:42 -0500

    Fixed bug where game froze if it couldnt award an achievemnt during cutscene

--- Assets/Scripts/Cutscenes/CutsceneEvents/AwardEventAchievement.cs
+++ Assets/Scripts/Cutscenes/CutsceneEvents/AwardEventAchievement.cs
@@ -2,7 +2,7 @@
  * FILE     : AwardEventAchievement.cs
  * AUTHOR   : Peter "prfctstrm479" Campbell
  * CREATION : 6/17/26
- * UPDATED  : 6/17/26
+ * UPDATED  : 6/22/26
  * 
  * DESC     : Awards an achievement as part of an event. CUrrently only supports steam, but can be
  *            modified to support other external achievement systems.
@@ -10,6 +10,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Steamworks;
 
 [CreateAssetMenu(fileName = "SteamAchievement", menuName = "Cutscene/Achievement", order = 10)]
 public class AwardEventAchievement : CutsceneEvent
@@ -29,6 +30,14 @@ public class AwardEventAchievement : CutsceneEvent
     {
         base.PlayEventFunction();
 
+        // Skips event if Steam can't be founs
+        if (!SteamAPI.Init())
+        {
+            eventComplete = true;
+            return;
+        }
+
+        // Awards Achievement
         AchievementManager.AwardAchievement(achName);
         eventComplete = true;
     }
