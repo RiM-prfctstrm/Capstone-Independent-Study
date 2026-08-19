f2810b65c853f677dea17701cc96e30d766c33a3
prfctstrm479 <pjpciii@gmail.com>
2026-08-19 11:04:54 -0500

    Beefed up bathroom and started working downstairs

--- Assets/Graphics/Tilesets/Indoor/Furniture.tsx
+++ Assets/Graphics/Tilesets/Indoor/Furniture.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Furniture" tilewidth="16" tileheight="16" tilecount="56" columns="8">
- <image source="Furniture.png" width="128" height="112"/>
+<tileset version="1.10" tiledversion="1.11.0" name="Furniture" tilewidth="16" tileheight="16" tilecount="80" columns="8">
+ <image source="Furniture.png" width="128" height="160"/>
  <tile id="0">
   <properties>
    <property name="unity:SortingLayer" value="Default"/>
